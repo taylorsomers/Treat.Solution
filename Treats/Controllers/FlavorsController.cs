e0a263c8733f92427bb7bbcd25393e6465055f08
Taylor Somers <tsomers@yandex.com>
2020-08-14 18:04:03 -0400

    Add Delete HTTPPost action

--- Treats/Controllers/FlavorsController.cs
+++ Treats/Controllers/FlavorsController.cs
@@ -90,5 +90,14 @@ namespace Treats.Controllers
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
       return View(thisFlavor);
     }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
