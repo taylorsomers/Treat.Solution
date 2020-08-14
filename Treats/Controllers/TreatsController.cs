157da00172fd513f1355ddb3b800a8b30f598e3b
Taylor Somers <tsomers@yandex.com>
2020-08-14 14:38:38 -0400

    Add basic Controller functionality and constructor

---
+++ Treats/Controllers/TreatsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Treats.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Treats.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly TreatsContext _db;
+
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, TreatsContext db)
+    {
+      _userManager = userManager;
+
+      _db = db;
+    }
+  }
+}
