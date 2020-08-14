0c47d7e95122525252b464161a1dd2fa4da4f96e
Taylor Somers <tsomers@yandex.com>
2020-08-14 16:37:49 -0400

    Update database to rename 'Name' column in 'Treats' table to 'TreatName'

--- Treats/Migrations/20200814203531_TreatName.Designer.cs
+++ Treats/Migrations/20200814203531_TreatName.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Treats.Models;
 
 namespace Treats.Migrations
 {
     [DbContext(typeof(TreatsContext))]
-    partial class TreatsContextModelSnapshot : ModelSnapshot
+    [Migration("20200814203531_TreatName")]
+    partial class TreatName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Treats/Migrations/20200814203531_TreatName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Treats.Migrations
+{
+    public partial class TreatName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TreatDescription",
+                table: "Treats",
+                newName: "TreatName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TreatName",
+                table: "Treats",
+                newName: "TreatDescription");
+        }
+    }
+}
