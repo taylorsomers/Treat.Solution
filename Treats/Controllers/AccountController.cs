2474ce69215b53de3b3763201343318ba44ff5df
Taylor Somers <tsomers@yandex.com>
2020-08-14 13:40:13 -0400

    Add Register HTTPPost action

--- Treats/Controllers/AccountController.cs
+++ Treats/Controllers/AccountController.cs
@@ -30,5 +30,20 @@ namespace Treats.Controllers
     {
       return View();
     }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
