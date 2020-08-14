434b9e98a54f895ea189b19bdb2348ecd4c0b407
Taylor Somers <tsomers@yandex.com>
2020-08-14 18:53:47 -0400

    Add Details HTTPGet method

--- Treats/Controllers/TreatsController.cs
+++ Treats/Controllers/TreatsController.cs
@@ -43,5 +43,14 @@ namespace Treats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(treat => treat.Flavors)
+          .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
   }
 }
