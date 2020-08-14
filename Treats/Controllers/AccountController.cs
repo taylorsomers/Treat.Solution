40ffbaf961714894838ef8cec1ed68366c6201a4
Taylor Somers <tsomers@yandex.com>
2020-08-14 13:56:17 -0400

    Add Login HTTPPost action

--- Treats/Controllers/AccountController.cs
+++ Treats/Controllers/AccountController.cs
@@ -50,5 +50,19 @@ namespace Treats.Controllers
     {
       return View();
     }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
