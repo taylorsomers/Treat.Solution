ccb2de06c12c942e86ead5db58a674e9925424a5
Taylor Somers <tsomers@yandex.com>
2020-08-14 12:20:22 -0400

    Add constructor and properties

---
+++ Treats/Models/Flavor.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Treats.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+        this.Treats = new HashSet<FlavorTreat>();
+    }
+
+    public int FlavorId { get; set; }
+
+    public string FlavorName { get; set; }
+    public virtual ICollection<FlavorTreat> Treats { get; set; }
+  }
+}
