de3749d895252df5fa8a9cd61bf99424ab0356f5
Taylor Somers <tsomers@yandex.com>
2020-08-14 19:47:47 -0400

    Add Delete HTTPPost method

--- Treats/Controllers/TreatsController.cs
+++ Treats/Controllers/TreatsController.cs
@@ -98,5 +98,14 @@ namespace Treats.Controllers
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
