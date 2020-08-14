640289c9aa4cc3e38a1ac427ecb4b00cdff13db8
Taylor Somers <tsomers@yandex.com>
2020-08-14 14:11:37 -0400

    Add basic Flavors Controller features and Index HTTPGet action

---
+++ Treats/Controllers/FlavorsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Treats.Models;
+
+namespace Treats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly TreatsContext _db;
+
+    public FlavorsController(TreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+  }
+}
